bb4142b399ba6ea72342461754656fbe9a5596ca
Luke Heineking <lheineking24@biomedscienceacademy.org>
2020-12-12 01:17:25 +0000

    I changed this from mad libs to my project. Put in some basic code which I will change later

--- main.py
+++ main.py
@@ -1,49 +1,40 @@
-"""
-MadLibs
-Author: Luke H
-Period/Core: C
+from urllib.parse import quote_plus
+
+import praw
+
+QUESTIONS = ["what is", "who is", "what are"]
+REPLY_TEMPLATE = "Hi"
 
 
-"""
-name = input("Enter name: ")
-adjective = input("Enter adjective: ")
-hum = input("Enter a hum (ex: bum du bum du dum: ")
-character = input("Enter another name: ")
-action = input("enter an action(no s at the end): ")
-character2 = input("Enter another name: ")
-character3 = input("Enter another name: ")
-vehicle = input("Enter vehicle: ")
-friend = input("enter synonym of friend: ")
-character4 = input("Enter another name: ")
+def main():
+    reddit = praw.Reddit(
+        user_agent="LMGTFY (by u/USERNAME)",
+        client_id="CLIENT_ID",
+        client_secret="CLIENT_SECRET",
+        username="USERNAME",
+        password="PASSWORD",
+    )
 
-print(name + ": " + hum + ", It is good day not to be" + " " + adjective + "!")
-print(character + " " + action + "s "  + name)
-print(character + ": Pow! you are" + " " + adjective + "!")
-print(name + ": I am "+adjective + "!")
-print(character2 + ": " + hum)
-print(character2 + ": Oh! " + name + " "  + "is " + adjective + "!")
-print(name + ": I am "+adjective + "!")
-print(character2 + ": why is " + name + " " + adjective + "?")
-print(character + ": I don't know!")
-print(name + ": I think it was-")
-print(f"{character} and {character2} : shh, you are {adjective}!")
-print(name + ": Ok.")
-print(character3 + ": Gets out of " + vehicle + "*, What's up " + friend + "s- What just happened?!")
-print(character + " and " + character2 + ": " + name + " " + "is " + adjective + "!")
-print(character3 + ": " + name + " " + "is " + adjective + "?!")
-print(character2 + ": Correct!")
-print(character2 + ": So, did you see the " + action + "er?")
-print("Everyone: No, sorry.")
-print(character2 + ": I will find him, I will capture him, and no one  will ever " + action + " again!")
-print("Everyone: Appluads")
-print(character3 + ": Ah, well that's nice.")
-print(character + ": I am so proud right now!")
-print(character4 + ": Attention!!!")
-print(character4 + ": " + name + " is " + adjective + "!")
-print(character2 + ": We know!")
-print(character4 + ": Who " + action + "ed him?")
-print(character2 + ": We don't know!")
-print(character4 + ": I will find clues!")
-print(character4 + ": What's that? An object? That thing is why " + name + " is " + adjective + "!")
-print("Everyone: " + name + " is " + adjective + "!?!" )
-print("END")
+    subreddit = reddit.subreddit("Subpythonproject")
+    for submission in subreddit.stream.submissions():
+        process_submission(submission)
+
+
+def process_submission(submission):
+    # Ignore titles with more than 10 words as they probably are not simple questions.
+    if len(submission.title.split()) > 10:
+        return
+
+    normalized_title = submission.title.lower()
+    for question_phrase in QUESTIONS:
+        if question_phrase in normalized_title:
+            url_title = quote_plus(submission.title)
+            reply_text = REPLY_TEMPLATE.format(url_title)
+            print(f"Replying to: {submission.title}")
+            submission.reply(reply_text)
+            # A reply has been made so do not attempt to match other phrases.
+            break
+
+
+if __name__ == "__main__":
+    main()
