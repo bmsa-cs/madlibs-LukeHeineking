bee870d0bed58d81260123fc0cbf13c105d4e8f2
Luke Heineking <lheineking24@biomedscienceacademy.org>
2020-12-16 00:36:10 +0000

    I realized that there was a much easier way to do this, Reddit has a built-in automod feature where you give it instructions and it reads them. These are those instructions

--- main.py
+++ main.py
@@ -1,40 +1,8 @@
-from urllib.parse import quote_plus
-
-import praw
-
-QUESTIONS = ["what is", "who is", "what are"]
-REPLY_TEMPLATE = "Hi"
-
-
-def main():
-    reddit = praw.Reddit(
-        user_agent="LMGTFY (by u/USERNAME)",
-        client_id="CLIENT_ID",
-        client_secret="CLIENT_SECRET",
-        username="USERNAME",
-        password="PASSWORD",
-    )
-
-    subreddit = reddit.subreddit("Subpythonproject")
-    for submission in subreddit.stream.submissions():
-        process_submission(submission)
-
-
-def process_submission(submission):
-    # Ignore titles with more than 10 words as they probably are not simple questions.
-    if len(submission.title.split()) > 10:
-        return
-
-    normalized_title = submission.title.lower()
-    for question_phrase in QUESTIONS:
-        if question_phrase in normalized_title:
-            url_title = quote_plus(submission.title)
-            reply_text = REPLY_TEMPLATE.format(url_title)
-            print(f"Replying to: {submission.title}")
-            submission.reply(reply_text)
-            # A reply has been made so do not attempt to match other phrases.
-            break
-
-
-if __name__ == "__main__":
-    main()
+---
+title: [Get rickrolled!, I hate automod!, die!, poop.]
+action: remove
+comment: Nooooooooooo! please don't!
+---
+author: classified
+comment: hi!!!
+---
